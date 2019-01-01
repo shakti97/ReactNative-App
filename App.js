11f3f4ceeecadadff05a68dadc896c3d885524a9
shakti97 <shaktisingh230797@gmail.com>
2019-01-01 21:02:01 +0530

    Initialize App

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+        /> */}
+        <View></View>
+        <Text>Awesome Component</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center'
+  },
+});
