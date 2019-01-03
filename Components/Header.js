52fdff90508dca70b14fd1f52bbbbcde131eaf1f
shakti97 <shaktisingh230797@gmail.com>
2019-01-03 23:29:56 +0530

    Refactor the Header Component

--- Components/Header.js
+++ Components/Header.js
@@ -4,21 +4,38 @@ import {View,Text,StyleSheet} from 'react-native';
 class Header extends React.Component{
     render(){
         return(
-            <View style={styles.headers}>
-                    <Text style={{'alignItems' : 'flex-start',width:'45%'}}>MENUBAR</Text>
-                    <Text style={{'alignItems' : 'center',width:'37%'}}>TITLE</Text>
-                    <Text style={{'alignItems' : 'flex-end'}}>HOME</Text>
+            <View style={styles.topView}>
+                <View style={styles.header}>
+                    <Text style={styles.menubar}>MENUBAR</Text>
+                    <Text style={styles.title}>TITLE</Text>
+                    <Text style={styles.home}>HOME</Text>
+                </View>
             </View>
+            // <View style={{backgroundColor : 'black'}}>
+            //     <View style={{backgroundColor : 'white'}}></View>
+            // </View>
         )
     }
 }
 const styles=StyleSheet.create({
-    headers:{
-    flex:1,
-    flexDirection:'row',
-    marginTop:'10%',
-    color: '#FFFFFF',
-    backgroundColor : '#FF0000',
+    topView:{
+        backgroundColor : '#87cefa'
+    },
+    header : {
+        flex:1,
+        flexDirection : 'row',
+        opacity : 10,
+        marginTop : '10%',
+        backgroundColor : '#FFF000'
+    },
+    menubar : {
+        'color' : 'white'
+    },
+    title : {
+        'color' : 'white'
+    },
+    home : {
+        'color' : 'white'
     }
 })
 export default Header;
